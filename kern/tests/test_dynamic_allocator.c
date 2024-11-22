60e3fca6f40e5ccec0fae0615a1dff90e414e6ce
Rahma <rahmakhattab07@gmail.com>
2024-11-22 14:09:32 +0200

    Feat: UH_initialization

--- kern/tests/test_dynamic_allocator.c
+++ kern/tests/test_dynamic_allocator.c
@@ -69,7 +69,8 @@ void test_initialize_dynamic_allocator()
 	*tmp_ptr = -1 ;
 	*(tmp_ptr+1) = 1 ;
 
-	uint32 initAllocatedSpace = 2*Mega;
+	//uint32 initAllocatedSpace = 2*Mega;
+	uint32 initAllocatedSpace = 2*sizeof(uint32);
 	initialize_dynamic_allocator(KERNEL_HEAP_START, initAllocatedSpace);
 
 
