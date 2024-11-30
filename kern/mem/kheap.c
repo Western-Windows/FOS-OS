2fe259825f143807a190c9d703a87fc5d522a6b5
Rehab Khaled El Bahnasy <rehab.khaled.bahnasy@gmail.com>
2024-11-30 02:18:46 +0200

    Implemented krealloc

--- kern/mem/kheap.c
+++ kern/mem/kheap.c
@@ -237,8 +237,50 @@ void *krealloc(void *virtual_address, uint32 new_size)
 {
 	//TODO: [PROJECT'24.MS2 - BONUS#1] [1] KERNEL HEAP - krealloc
 	// Write your code here, remove the panic and write your code
-	return NULL;
-	panic("krealloc() is not implemented yet...!!");
+	uint32 size = ROUNDUP(new_size,PAGE_SIZE);
+	if(virtual_address==NULL && new_size){
+			return kmalloc(new_size);
+		}
+		if(virtual_address==NULL && (new_size==0)){
+			return NULL;
+		}
+		if((new_size==0)){
+			kfree(virtual_address);
+			return NULL;
+		}
+		if(virtual_address <= (hardLimit)){
+			if(new_size <= DYN_ALLOC_MAX_BLOCK_SIZE){
+				return realloc_block_FF(virtual_address,new_size);
+			}
+			kfree(virtual_address);
+			return kmalloc(new_size);
+		}
+		uint32 vaRoundDown = ROUNDDOWN((uint32)virtual_address,PAGE_SIZE);
+		uint32 start_index = (uint32)((vaRoundDown - (uint32)((char*)hardLimit+PAGE_SIZE)))>>12;
+		int pages = pageStatus[start_index];
+		int old_size = pages*PAGE_SIZE;
+		if(old_size == size)
+			return virtual_address;
+		if(size<old_size){
+			pageStatus[start_index] = size/PAGE_SIZE;
+			kfree((void*)((char*)virtual_address+old_size));
+			return virtual_address;
+		}
+		uint32 new_size_pages = size/PAGE_SIZE;
+		uint32 rem = new_size_pages - pages;
+		uint32 check = start_index + pages;
+		for(int i = check; i < check+rem;i++){
+			if(~pageStatus[i]){
+				void* va = kmalloc(new_size);
+				kfree(virtual_address);
+				return va;
+			}
+		}
+		uint32 startVa = ((uint32)((char*)hardLimit+(PAGE_SIZE*(check+1))));
+		uint32 limit = ((uint32)((char*)hardLimit+(PAGE_SIZE*(check+rem))));
+		allocateMapFrame(startVa , limit);
+		pageStatus[start_index] = size/PAGE_SIZE;
+		return virtual_address;
 }
 
 /********************Helper Functions***************************/
