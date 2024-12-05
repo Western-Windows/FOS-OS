233d6bfd75372ed336f3b9d160fae40e9552e48b
Haneen Khalifa <haneenhany1205@gmail.com>
2024-12-06 01:53:56 +0200

    feat: Implement create_semaphore

--- lib/semaphore.c
+++ lib/semaphore.c
@@ -1,14 +1,39 @@
 // User-level Semaphore
 
 #include "inc/lib.h"
+#include "inc/queue.h"
 
 struct semaphore create_semaphore(char *semaphoreName, uint32 value)
 {
 	//TODO: [PROJECT'24.MS3 - #02] [2] USER-LEVEL SEMAPHORE - create_semaphore
 	//COMMENT THE FOLLOWING LINE BEFORE START CODING
-	panic("create_semaphore is not implemented yet");
+	//panic("create_semaphore is not implemented yet");
 	//Your Code is Here...
+
+	// Create shared object
+	void* va = smalloc(semaphoreName, sizeof(struct semaphore), 1);
+	if (va == NULL)
+	{
+		return NULL;
+	}
+	struct semaphore* sem = (struct semaphore*) va;
+
+	// Initialize semaphore data
+	for (int i = 0; i < sizeof(semaphoreName) / sizeof(int); i++)
+	{
+		sem->semdata->name[i] = semaphoreName[i];
+	}
+	sem->semdata->count=value;
+	sem->semdata->lock=0;
+
+	if(&(sem->semdata->queue) != NULL)
+	{
+		LIST_INIT(&(sem->semdata->queue));
+	}
+
+	return *sem;
 }
+
 struct semaphore get_semaphore(int32 ownerEnvID, char* semaphoreName)
 {
 	//TODO: [PROJECT'24.MS3 - #03] [2] USER-LEVEL SEMAPHORE - get_semaphore
