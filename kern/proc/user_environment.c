b3f38c75d266cb45cebef0368d89ddf0d31dc0a2
Rehab Khaled El Bahnasy <rehab.khaled.bahnasy@gmail.com>
2024-12-20 15:54:39 +0200

    Update user_environment.c
    Co-Authored-By: Youssefproof771 <113691824+Youssefproof771@users.noreply.github.com>
    Co-Authored-By: Ziad Khaled <130778848+Ziad-Khaled11@users.noreply.github.com>

--- kern/proc/user_environment.c
+++ kern/proc/user_environment.c
@@ -514,7 +514,8 @@ void env_free(struct Env *e)
 	//********************************************************************//
 	struct Share *share = NULL;
 	LIST_FOREACH(share, &AllShares.shares_list){
-		acquire_spinlock(&AllShares.shareslock);
+		if (!holding_spinlock(&AllShares.shareslock))
+			acquire_spinlock(&AllShares.shareslock);
 		if (share->ownerID == e->env_id){
 			release_spinlock(&AllShares.shareslock);
 			free_share(share);
@@ -528,7 +529,10 @@ void env_free(struct Env *e)
 	//********************************************************************//
 	//    f) Freeing All page tables in the entire user virtual memory
 	//********************************************************************//
-
+	for(uint32 i = USER_HEAP_START; i < USER_HEAP_MAX;i+=PAGE_SIZE){
+		unmap_frame(e->env_page_directory, i);
+	}
+//	cprintf("no of page indexes : %d\n",(USER_HEAP_MAX - USER_HEAP_START)/PAGE_SIZE);
 	uint32 USER_BOTTOM = PDX(USER_TOP);
 	//cprintf("User_bottom %d\n", USER_BOTTOM);
 	for (int index = 0 ; index < USER_BOTTOM ; index ++)
@@ -544,12 +548,12 @@ void env_free(struct Env *e)
 		uint32 *pageTablePtr = (uint32 *)kheap_virtual_address(pa);
 		uint32 va;
 
-		for (int index = 0 ; index < NPTENTRIES ; index ++)
+		for (int j = 0 ; j < NPTENTRIES ; j ++)
 		{
-			if (pageTablePtr[index] != 0)
+			if (pageTablePtr[j] != 0)
 			{
 				//cprintf("Page table ptr index %x \n", pageTablePtr[index]);
-				uint32 pa = EXTRACT_ADDRESS(pageTablePtr[index]);
+				uint32 pa = EXTRACT_ADDRESS(pageTablePtr[j]);
 				va = kheap_virtual_address(pa);
 				//cprintf("VA of a page table entry = %x\n", va);
 				unmap_frame(e->env_page_directory, va);
@@ -564,14 +568,14 @@ void env_free(struct Env *e)
 		pd_clear_page_dir_entry(e->env_page_directory, va);
 	}
 
-	cprintf("All page tables have been freed.\n");
+//	cprintf("All page tables have been freed.\n");
 
 	//********************************************************************//
 	//                     g) Freeing directory table
 	//********************************************************************//
 
 	kfree(e->env_page_directory);
-	cprintf("Page directory has been freed.\n");
+//	cprintf("Page directory has been freed.\n");
 
 
 	// [h] remove this program from the page file
